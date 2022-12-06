4faf969e2279bbff6cefa8f3e84b229a81279be1
Volker Homburg <volker.homburg@arvato-scs.com>
2022-12-06 08:26:33 +0100

    Day 2 Part Two.

--- 2/main.go
+++ 2/main.go
@@ -36,16 +36,29 @@ func main() {
 }
 
 func createScoreMap() {
-
+	// Part Two
 	scoreMap = map[string]int{
-		"A X": 4,
-		"A Y": 8,
-		"A Z": 3,
+		"A X": 3,
+		"A Y": 4,
+		"A Z": 8,
 		"B X": 1,
 		"B Y": 5,
 		"B Z": 9,
-		"C X": 7,
-		"C Y": 2,
-		"C Z": 6,
+		"C X": 2,
+		"C Y": 6,
+		"C Z": 7,
 	}
+
+	// Part One
+	//	scoreMap = map[string]int{
+	//		"A X": 4,
+	//		"A Y": 8,
+	//		"A Z": 3,
+	//		"B X": 1,
+	//		"B Y": 5,
+	//		"B Z": 9,
+	//		"C X": 7,
+	//		"C Y": 2,
+	//		"C Z": 6,
+	//	}
 }
