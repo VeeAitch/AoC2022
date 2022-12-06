32d3083d3cf40cd7a7ed50929869ac2f679606d7
Volker Homburg <volker.homburg@arvato-scs.com>
2022-12-06 14:57:04 +0100

    Day 4 Part Two.

--- 4/main.go
+++ 4/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("\nDay 4 Part One: %v", partOne())
+	fmt.Printf("\nDay 4 Part One: %v\n", partOne())
 	fmt.Printf("\nDay 4 Part Two: %v", partTwo())
 }
 
@@ -32,8 +32,6 @@ func partOne() int {
 		elveTwoEnd, _ := strconv.Atoi(matches[3])
 
 		if (elveOneStart >= elveTwoStart && elveOneEnd <= elveTwoEnd) || (elveOneStart <= elveTwoStart && elveOneEnd >= elveTwoEnd) {
-			fmt.Println(pair)
-			fmt.Printf("Matches: %v\n", matches)
 			counter++
 		}
 	}
@@ -42,22 +40,27 @@ func partOne() int {
 }
 
 func partTwo() int {
-	// readFile, err := os.Open("input")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fileScanner := bufio.NewScanner(readFile)
-	// fileScanner.Split(bufio.ScanLines)
+	readFile, err := os.Open("input")
+	if err != nil {
+		fmt.Println(err)
+	}
+	counter := 0
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
 
-	// prioSum := 0
-	// rowcount := 1
-	// elvecount := 0
-	// group := make([]string, 3)
+	for fileScanner.Scan() {
+		pair := fileScanner.Text()
+		re := regexp.MustCompile(`\d+`)
+		matches := re.FindAllString(pair, -1)
+		elveOneStart, _ := strconv.Atoi(matches[0])
+		elveOneEnd, _ := strconv.Atoi(matches[1])
+		elveTwoStart, _ := strconv.Atoi(matches[2])
+		elveTwoEnd, _ := strconv.Atoi(matches[3])
 
-	// for fileScanner.Scan() {
-	// 	rucksack := fileScanner.Text()
-
-	// }
-	// readFile.Close()
-	return 0
+		if ((elveOneStart >= elveTwoStart && elveOneStart <= elveTwoEnd) || (elveOneEnd <= elveTwoEnd && elveOneEnd >= elveTwoStart)) || ((elveOneStart >= elveTwoStart && elveOneEnd <= elveTwoEnd) || (elveOneStart <= elveTwoStart && elveOneEnd >= elveTwoEnd)) {
+			counter++
+		}
+	}
+	readFile.Close()
+	return counter
 }
